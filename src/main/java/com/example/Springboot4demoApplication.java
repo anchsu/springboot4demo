aad4cc270cf42893483c901c132b03d001685d39
U-HTM-GROUP\ex200 <ex200@MCZC42426RD.htm-group.com>
2017-03-13 07:52:19 +0100

    first commit

---
+++ src/main/java/com/example/Springboot4demoApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot4demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot4demoApplication.class, args);
+	}
+}
